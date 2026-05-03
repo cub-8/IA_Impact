4a934e24ceafbb78e933c2b0957e2dd3ed6a7ad5
cub8 <caoc.cubas@gmail.com>
2026-05-02 20:20:52 -0600

    filter by geo area

--- app.py
+++ app.py
@@ -1,6 +1,29 @@
 import streamlit as st
 import kagglehub as kh
+import pandas as pd
 import matplotlib.pyplot as plt
 
-from kagglehub import KaggleHubDatasetAdapter
+from kagglehub import KaggleDatasetAdapter
+
+df = kh.dataset_load(
+  KaggleDatasetAdapter.PANDAS,
+  "guriya79/how-ai-is-changing-student-life",
+  "AI_Student_Life_Pakistan_2026.csv"
+)
+
+st.title("Impacto de la IA en la vida estudiantil en Pakistán") 
+
+# st.write(df.head())
+
+#1.1
+
+city = st.selectbox("Selecciona una ciudad", ["Todas"] + list(df["City"].unique()))
+
+bar_impact_ia = df
+
+if city != "Todas":
+    bar_impact_ia = bar_impact_ia[bar_impact_ia["City"] == city]
+    st.bar_chart(bar_impact_ia.groupby(["AI_Tool_Used", "Impact_on_Grades"]).size().unstack())
+else:
+    st.bar_chart(bar_impact_ia.groupby(["AI_Tool_Used", "Impact_on_Grades"]).size().unstack())
 
