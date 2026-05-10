dca71eda50a47d540a4d839973f3280c70557167
cub8 <caoc.cubas@gmail.com>
2026-05-09 20:31:07 -0600

    anomaly analisis

--- app.py
+++ app.py
@@ -16,6 +16,8 @@ st.title("Impacto de la IA en la vida estudiantil en Pakistán")
 st.write(df.head())
 
 #1.1
+st.subheader("Impacto de la IA en las calificaciones por ciudad")
+
 city = st.selectbox("Selecciona una ciudad", ["Todas"] + list(df["City"].unique()))
 
 bar_impact_ia = df
@@ -27,8 +29,36 @@ else:
     st.bar_chart(bar_impact_ia.groupby(["AI_Tool_Used", "Impact_on_Grades"]).size().unstack())
 
 #1.2
+st.subheader("Impacto de la IA en las calificaciones por propósito de uso")
+
 purpose = st.radio("Selecciona el uso de IA", df["Purpose"].unique())
 
 bar_purpose_ia = df[df["Purpose"] == purpose]
 
 st.bar_chart(bar_purpose_ia["Impact_on_Grades"].value_counts())
+
+#1.3
+st.subheader("Alumnos con alto nivel de satisfacción y ligera disminución en las calificaciones")
+
+high_decline = df[(df["Impact_on_Grades"] == "Slight Decline") & (df["Satisfaction_Level"] == "High")]
+
+num_high_decline = len(high_decline)
+
+st.write("Total de estudiantes con satisfacción alta y ligera disminución en las calificaciones:", num_high_decline)
+
+min_hours = float(df["Daily_Usage_Hours"].min())
+max_hours = float(df["Daily_Usage_Hours"].max())
+
+hours = st.slider("Rango de horas de uso diario", min_value=min_hours, max_value=max_hours, value=(min_hours, max_hours))
+
+high_decline_hours = high_decline[(high_decline["Daily_Usage_Hours"] >= hours[0]) & (high_decline["Daily_Usage_Hours"] <= hours[1])]
+
+ia_students = (
+    high_decline_hours["AI_Tool_Used"]
+    .value_counts()
+    .reset_index()
+)
+ia_students.columns = ["Herramienta", "Cantidad de estudiantes"]
+st.dataframe(ia_students)
+
+
